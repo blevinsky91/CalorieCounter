2cc46d5a9a2329daee25ad81a563ebbf39a7230a
Brandon Levinsky <blevinsky91@gmail.com>
2023-08-28 12:04:55 -0400

    init

---
+++ Calorie Counter/Calorie Counter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Calorie_Counter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
