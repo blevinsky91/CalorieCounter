00a05f0d460106e9832b72b29917af736681a6e5
Brandon Levinsky <blevinsky91@gmail.com>
2023-09-14 11:08:47 -0400

    updated controller, view, and model for User

--- Calorie Counter/Calorie Counter/Controllers/MainController.cs
+++ Calorie Counter/Calorie Counter/Controllers/MainController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Calorie_Counter.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,7 @@ namespace Calorie_Counter.Controllers
 {
     public class MainController : Controller
     {
+
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -22,9 +24,39 @@ namespace Calorie_Counter.Controllers
             return View(userModel);
         }
 
-        public ActionResult CalculatorResult()
+        public ActionResult CalculatorResult(Users userModel)
         {
+            double bmr = 0;
+            if (userModel.IsMale)
+            {
+                bmr =
+                    66 +
+                    (13.7 * userModel.Weight) +
+                    (5 * userModel.Height) -
+                    (6.76 * userModel.Age);
+            }
+            if (userModel.IsFemale)
+            {
+                bmr =
+                    655 +
+                    (9.6 * userModel.Weight) +
+                    (1.8 * userModel.Height) -
+                    (4.7 * userModel.Age);
+            }
 
+            userModel.LifestyleRates.Clear();
+            userModel.LifestyleRates.Add(
+                new KeyValuePair<string, int>("Sedentry", (int)(bmr * 1.2)));
+            userModel.LifestyleRates.Add(
+                new KeyValuePair<string, int>("Lightly Active", (int)(bmr * 1.375)));
+            userModel.LifestyleRates.Add(
+                new KeyValuePair<string, int>("Moderately Active", (int)(bmr * 1.55)));
+            userModel.LifestyleRates.Add(
+                new KeyValuePair<string, int>("Very Active", (int)(bmr * 1.725)));
+            userModel.LifestyleRates.Add(
+                new KeyValuePair<string, int>("Extra Active", (int)(bmr * 1.9)));
+
+            return View(userModel);
         }
     }
 }
--- Calorie Counter/Calorie Counter/Models/Users.cs
+++ Calorie Counter/Calorie Counter/Models/Users.cs
@@ -3,14 +3,21 @@ namespace Calorie_Counter.Models
 {
 	public class Users
 	{
-		public int Id { get; set; }
+        public Users()
+        {
+            //LifestyleRates = new List<KeyValuePair>();
+        }
+
+        public int Id { get; set; }
 		public string UserName { get; set; }
 		public string Password { get; set; }
 		public int Age { get; set; }
 		public string Email { get; set; }
 		public int Weight { get; set; }
 		public int Height { get; set; }
-		public enum Gender { get; set; }
-	}
+		public bool IsMale { get; set; }
+		public bool IsFemale { get; set; }
+		public IList<KeyValuePair<string, int>> LifestyleRates { get; set; }
+    }
 }
 
